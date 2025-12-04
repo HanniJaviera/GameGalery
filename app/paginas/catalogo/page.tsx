abd8f51a5a2838f84907eea93049b4edc7b7a087
Hanni Javiera <166668626+HanniJaviera@users.noreply.github.com>
2025-12-04 01:10:55 +0000

    cambio en catalogo

--- app/paginas/catalogo/page.tsx
+++ app/paginas/catalogo/page.tsx
@@ -1,18 +1,70 @@
 "use client";
+import { useState, useEffect } from "react";
+import { Container, Row, Col, Spinner, Alert } from "react-bootstrap";
 import GameCard from "../../../components/GameCard";
-import { juegosData } from "../../juegos";
-import { Container, Row, Col } from "react-bootstrap";
+import { Juego } from "@/app/juegos";
 
 export default function CatalogoPage() {
+  const [juegos, setJuegos] = useState<Juego[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProductos = async () => {
+      try {
+
+        const url = `${process.env.NEXT_PUBLIC_API_PRODUCTS}/api/productos`;
+
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error("Error al obtener los datos del servidor");
+        }
+
+        const data = await response.json();
+
+        setJuegos(data);
+      } catch (err) {
+        console.error(err);
+        setError("Hubo un problema cargando el catálogo.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProductos();
+  }, []);
+
+  if (loading)
+    return (
+      <Container style={{ paddingTop: "100px", textAlign: "center" }}>
+        <Spinner animation="border" variant="light" />
+        <h3 className="text-white mt-3">Cargando...</h3>
+      </Container>
+    );
+
+  if (error)
+    return (
+      <Container style={{ paddingTop: "100px" }}>
+        <Alert variant="danger">{error}</Alert>
+      </Container>
+    );
+
   return (
     <Container style={{ paddingTop: "80px" }}>
-      <h1 className="text-center my-4 white">Catálogo de Juegos</h1>
+      <h1 className="text-center my-4 text-white">Catálogo de Juegos</h1>
       <Row className="justify-content-center g-4">
-        {juegosData.map((juego) => (
-          <Col lg={3} md={4} sm={6} xs={12} key={juego.id}>
-            <GameCard juego={juego} />
-          </Col>
-        ))}
+        {juegos.length > 0 ? (
+          juegos.map((juego) => (
+            <Col lg={3} md={4} sm={6} xs={12} key={juego.id}>
+              <GameCard juego={juego} />
+            </Col>
+          ))
+        ) : (
+          <div className="text-white text-center">
+            No hay productos disponibles.
+          </div>
+        )}
       </Row>
     </Container>
   );
